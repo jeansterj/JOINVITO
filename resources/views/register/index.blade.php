abd4b055e4f6b19e6761d53031b6ebfc32a5d6df
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-02-21 20:20:51 +0100

    página a medias
    página a medias

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -13,13 +13,24 @@
     <h1>REGISTER</h1>
     <h2>CHOOSE USER TYPE</h2>
     <div class="forms">
-        <div class="rider">
+        <div class="rider formSelectorIcon">
             <img src="../public/img/rider_icon.png" alt="rider_icon.png" width="132px">
             <figcaption>RIDER</figcaption>
         </div>
-        <div class="supplier">
+        <div class="supplier formSelectorIcon">
             <img src="../public/img/supplier_icon.png" alt="rider_icon.png" width="111px">
             <figcaption>SUPPLIER</figcaption>
         </div>
     </div>
+    <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-light">Submit</button>
+    </form>
 @endsection
