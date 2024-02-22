c560c05f00c4a5bf62bf1e151cc79a58a8d1987c
filiprendon <jdasilvao2223@politecnics.barcelona>
2024-02-22 19:51:58 +0100

    database

--- database/db.sql
+++ database/db.sql
@@ -19,7 +19,6 @@ primer_apellido varchar(100) not null,
 primary key(id_rider),
 FOREIGN KEY (id_rider) REFERENCES usuarios(id_usu)
 );
-
 create table proveedores(
 id_prov int auto_increment,
 nombre varchar(50) not null,
@@ -33,41 +32,28 @@ primary key(id_prov),
 FOREIGN KEY proveedores(id_prov) REFERENCES usuarios(id_usu)
 );
 
-<<<<<<< HEAD
--- create table centros_sociales(
--- id_centro int auto_increment,
--- nombre varchar(50) not null,
--- direccion varchar(250) not null,
--- piso varchar(30),
--- ciudad varchar(250) not null,
--- cp varchar(5) not null,
--- primary key(id_centro),
--- FOREIGN KEY (id_centro) REFERENCES usuarios(id_usu)
--- );
-
-create table menus(
-=======
-create table menus_predeterminados(
->>>>>>> main
-id_menu int auto_increment,
-bebida varchar(250) not null,
-plato1 varchar(250) not null,
-plato2 varchar(250) not null,
-primary key(id_menu)
+create table centros_sociales(
+id_centro int auto_increment,
+nombre varchar(50) not null,
+direccion varchar(250) not null,
+piso varchar(30),
+ciudad varchar(250) not null,
+cp varchar(5) not null,
+primary key(id_centro),
+FOREIGN KEY (id_centro) REFERENCES usuarios(id_usu)
 );
 
-create table packs(
-id_pack int auto_increment,
+
+create table menus(
+id_menu int auto_increment,
 bebida varchar(250),
 plato1 varchar(250),
 plato2 varchar(250),
-id_menu int,
 cantidad_packs int not null,
 id_prov int not null,
 fecha_alta date not null,
 FOREIGN KEY (id_prov) REFERENCES proveedores(id_prov),
-FOREIGN KEY (id_menu) REFERENCES menus_predeterminados(id_menu),
-primary key(id_pack)
+primary key(id_menu)
 );
 
 create table puntos_entrega(
@@ -88,34 +74,22 @@ primary key(id_punto)
 create table pedidos(
 id_pedido int auto_increment,
 id_rider int not null,
-id_proveedor int not null,
-id_pack int not null,
+id_menu int not null,
 cantidad_packs int not null,
 fecha date not null,
-<<<<<<< HEAD
-entregado_proveedor boolean not null,
-estado_pedido enum("Entregado","No_entregado"),
-id_punto int,
-=======
 entregado_a_rider boolean not null,
->>>>>>> main
 FOREIGN KEY (id_rider) REFERENCES riders(id_rider),
-FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_prov),
-FOREIGN KEY (id_pack) REFERENCES packs(id_pack),
+FOREIGN KEY (id_menu) REFERENCES menus(id_menu),
 primary key(id_pedido)
 );
 
 create table entregas(
 id_entrega int auto_increment,
-id_rider int not null,
-id_proveedor int not null,
 id_pedido int not null,
 id_punto int not null,
 cantidad_packs int not null,
 fecha date not null,
 entregado boolean,
-FOREIGN KEY (id_rider) REFERENCES riders(id_rider),
-FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_prov),
 FOREIGN KEY (id_punto) REFERENCES puntos_entrega(id_punto),
 FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
 primary key(id_entrega)
@@ -123,11 +97,11 @@ primary key(id_entrega)
 
 create table historico_entregas(
 id_entrega int auto_increment,
-id_rider int not null,
-id_proveedor int not null,
-id_punto int,
+id_pedido int not null,
+id_punto int not null,
 cantidad_packs int not null,
 fecha date not null,
+entregado boolean,
 primary key(id_entrega)
 );
 
@@ -143,15 +117,8 @@ primary key(id_fav)
 create table notis_usuario(
 id_noti int auto_increment,
 mensaje varchar(250) not null,
-id_prov int not null,
-id_rider int not null,
+id_fav int not null,
 leido boolean,
-primary key(id_noti),
-FOREIGN KEY (id_rider) REFERENCES riders(id_rider),
-FOREIGN KEY (id_prov) REFERENCES proveedores(id_prov)
-);
-
-create table user_settings(
-id_usu int not null,
-idioma varchar(3) not null
+FOREIGN KEY (id_fav) REFERENCES favoritos(id_fav),
+primary key(id_noti)
 );
