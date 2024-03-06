221cb2c3c081f6fbf93a5ff8da2e4b5fd4e00baa
Javier <jcastillop2223@politecnics.barcelona>
2024-03-06 20:12:42 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,6 @@ Route::resource('puntos', PuntoController::class);
 
 Route::resource('usuario', UsuarioController::class);
 
-Route::get('rider/1/{lat?}/{long?}/update', [RiderController::class,'updateLocation'])->name('rider.updateLocation');
-
 Route::get('rider-menu-selection', [RiderController::class,'showFavoritesNearBy']);
 
 Route::resource('pedido', PedidoController::class);
@@ -56,4 +54,18 @@ Route::get('edit-rider', function () {
 
 Route::get('addLocation', function () {
     return view('rider.addLocation');
+});
+
+
+Route::get('/login', [UsuarioController::class,'showLogin'])->name('login');
+Route::post('/login', [UsuarioController::class,'login']);
+Route::get('/logout', [UsuarioController::class,'logout']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function(){
+
+        $user = Auth::user();
+
+        return view('home',compact('user'));
+    });
 });
