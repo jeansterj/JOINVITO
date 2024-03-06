209a7e57c826657ac07cc1cfd8491fc8d651c422
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-03-06 19:08:37 +0100

    bd
    bd

--- database/db.sql
+++ database/db.sql
@@ -17,7 +17,7 @@ insert into roles values(4,'proveedor');
 
 create table usuarios(
 id_usu int auto_increment,
-email varchar(50) not null,
+email varchar(50) not null unique,
 pass_usu varchar(255) not null,
 id_rol int not null,
 FOREIGN KEY (id_rol) REFERENCES roles(id_rol),
