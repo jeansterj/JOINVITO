699a81f8da67f60f6e9d38f1b5228e95f003904e
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-03-19 19:13:33 +0100

    acabado el footer
    acabado

--- resources/js/footerAndMenuCollision.js
+++ resources/js/footerAndMenuCollision.js
@@ -3,25 +3,12 @@ let menu = document.getElementById('floatingMenu')
 
 let fheight = footer.offsetHeight
 
-let height = menu.offsetHeight
+window.onscroll = function() {
+   update()
+};
 
-let footerY = footer.offsetTop
-
-let windowHeight = window.innerHeight
-
-let windowNoFooter = windowHeight - fheight
-
-document.onscroll = function () {
-    menu.style.bottom = document.height
-    menu.style.top = window.height
-    update()
-}
 function update() {
-    // window.innerHeight
-    let windowBottom = document.body.scrollTop + height 
-    console.log('window : ' + windowBottom)
-    console.log('footer: ' + (footerY))
-    if (windowBottom >= (footerY)) {
+    if ((window.innerHeight + document.body.scrollTop) >= document.body.offsetHeight) {
         menu.style.animation = 'menuTouchFooter 0.5s'
         menu.onanimationend = function () {
             menu.style.bottom = fheight
