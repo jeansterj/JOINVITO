236d36d76ecdf2d844018b116b9938f88ba1b355
Javier <jcastillop2223@politecnics.barcelona>
2024-04-10 20:25:45 +0200

    Charts

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -14,18 +14,18 @@ class ChartController extends Controller
     {
 
         // $entregas = Pedido::with('entregas')->where('id_rider',$userId)->get();
-        $entregas = Pedido::with('entregas')
-                        ->selectRaw('MONTH(fecha) AS mes,YEAR(fecha) AS anyo, COUNT(*) AS total_entregas')
-                        ->where('id_rider', $userId)
-                        ->groupByRaw('YEAR(fecha), MONTH(fecha)')
-                        ->get();
+        // $entregas = Pedido::with('entregas')
+        //                 ->selectRaw('MONTH(fecha) AS mes,YEAR(fecha) AS anyo, COUNT(*) AS total_entregas')
+        //                 ->where('id_rider', $userId)
+        //                 ->groupByRaw('YEAR(fecha), MONTH(fecha)')
+        //                 ->get();
         
         // Replace this with your actual data retrieval logic
-        // $data = [
-        //     'labels' => ['January', 'February', 'March', 'April', 'May'],
-        //     'data' => [65, 59, 80, 81, 56],
-        // ];
+        $data = [
+            'labels' => ['January', 'February', 'March', 'April', 'May'],
+            'data' => [65, 59, 80, 81, 56],
+        ];
 
-        return ChartResource::collection($entregas);
+        return ChartResource::collection($data);
     }
 }
