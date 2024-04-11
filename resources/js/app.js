4040e818dd3c6d880fc3ba0cc012364229080f32
filiprendon <jdasilvao2223@politecnics.barcelona>
2024-04-11 17:37:48 +0200

    g

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@ import OrdersProvider from './components/OrdersProvider.vue'
 import OrdersRider from './components/OrdersRider.vue'
 import Map from './components/Map.vue'
 import Stats from './components/Stats.vue'
-import { setLanguage, getSelectedLanguage } from './translations.js';
 
 
 createApp(MenusProvider).mount('#divMenuList')
@@ -16,9 +15,3 @@ createApp(OrdersRider).mount('#ordersRider')
 createApp(Map).mount('#map')
 createApp(Stats).mount('#stats')
 
-document.addEventListener ('DOMContentLoaded', function () {
-
-    const selectedLanguage = getSelectedLanguage();
-    
-    setLanguage(selectedLanguage);
-    })
