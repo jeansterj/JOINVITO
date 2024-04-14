788591cbb5c59d382c49759ff753f7b728175689
jeansterj <jeansterj@gmail.com>
2024-04-14 19:02:40 +0200

    s

--- resources/views/layouts/navbarLayout.blade.php
+++ resources/views/layouts/navbarLayout.blade.php
@@ -1,8 +1,8 @@
 @section('navbarContent')
     <div class="navbarBody">
 
-        <h1 class="foodForHomelessTitle">FOOD FOR HOMELESS</h1>
-        <p class="joinAsRiderText">JOIN AS A RIDER OR SUPPLIER FOR HELP PEPOLE</p>
+        <h1 class="foodForHomelessTitle" >FOOD FOR HOMELESS</h1>
+        <p class="joinAsRiderText" >JOIN AS A VOLUNTARI OR SUPPLIER FOR HELP PEPOLE</p>
     </div>
 @endsection
 @section('img')
