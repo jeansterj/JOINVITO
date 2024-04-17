dde2260e17d883cc5db1230dd4f914dcc40d90f3
Cbas420 <grepi.seti@gmail.com>
2024-04-17 18:43:20 +0200

    das
    das

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -141,13 +141,13 @@
             @yield('contentHome')
         </div>
         <div class="accesibilityDiv" id="accesibilityDiv">
-            <img src="../public/img/accesibility_icon2.png" id="accesibilityIcon" data-toggle="modal" data-target="#exampleModal"> 
+            <img src="../public/img/accesibility_icon2.png" id="accesibilityIcon" data-toggle="modal" data-target="#acModal"> 
         </div>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="acModal" tabindex="-1" role="dialog" aria-labelledby="acModal" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Accesibility Options</h5>
+                  <h5 class="modal-title" id="acModal">Accesibility Options</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
