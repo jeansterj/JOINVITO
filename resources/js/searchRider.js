5d8c37e1bb0f5573ae371a5ec3c3fe29c704ad1d
Fran Garcia <francisco.garcia.arroyo@gmail.com>
2024-04-20 15:52:15 +0200

    add pedido from admin

---
+++ resources/js/searchRider.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('searchInputRider');
+    const dataRows = document.querySelectorAll('#dataBodyRider tr');
+    const noResultsMessage = '<tr><td colspan="3">No data to display</td></tr>';
+    let originalTable = ''; // Almacena una copia original de la tabla
+    let riderName = document.getElementById('rider');
+    let riderId = document.getElementById('idrider');
+
+    // Función para restaurar la tabla original
+    function restoreOriginalTable() {
+      dataRows.forEach(row => row.style.display = '');
+    }
+
+    // Manejar el evento de entrada en el campo de búsqueda
+    if(searchInput != null){
+        searchInput.addEventListener('input', function() {
+            const searchText = searchInput.value.trim().toLowerCase();
+      
+            // Si la tabla original aún no se ha guardado, guardarla
+            if (!originalTable) {
+              originalTable = Array.from(dataRows).map(row => row.outerHTML).join('');
+            }
+      
+            // Filtrar las filas de la tabla y actualizar la tabla
+            if (searchText) {
+              restoreOriginalTable();
+              let hasResults = false;
+              dataRows.forEach(row => {
+                const rowData = row.children[0].textContent.trim().toLowerCase();
+                if (rowData.includes(searchText)) {
+                  row.style.display = '';
+                  let hasResults = true;
+                } else {
+                  row.style.display = 'none';
+                }
+              });
+      
+              if (!hasResults) {
+                  dataRows[0].parentElement.innerHTML = noResultsMessage;
+              }
+      
+            } else {
+              // Si el campo de búsqueda está vacío, restaurar la tabla original
+              restoreOriginalTable();
+            }
+        });
+    }
+    
+
+    dataRows.forEach(row => {
+        row.addEventListener('click',(event) => {
+
+            const trElement = event.target.parentElement;
+            riderName.textContent = trElement.getAttribute('data-nameRider');
+            riderId.value = trElement.getAttribute('data-idRider');
+        })
+    })
+
+  });
--- resources/views/admin/asignarPedidosRider.blade.php
+++ resources/views/admin/asignarPedidosRider.blade.php
@@ -3,79 +3,90 @@
 @section('content')
 
     <div class="container">
-        <div class="container adminHome">
+        <div class="container adminHome addPedido">
             <div class="row margin-top">
                 <h2>ASIGNACIÓN DE PEDIDO A UN RIDER</h2>
             </div>
             <div class="row">
                 <div class="col">
                     <h3>SELECT MENU</h3>
-                    <div class="containe bg-body-tertiary">
-                        <div class="accordion" id="accordionExample">
-                            @foreach ($providers as $provider)
-                                <div class="accordion-item">
-                                    <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $provider->id_prov }}" aria-expanded="false" aria-controls="collapseOne">
-                                        {{ $provider->nombre }}
-                                    </button>
-                                    </h2>
-                                        <div id="collapse{{ $provider->id_prov }}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                                            @foreach ($provider->menus as $menu)
-                                                <div class="accordion-body">
-                                                    <div class="row">
-                                                        <div class="col-4">
-                                                            <strong>{{ $menu->nombre_menu }}</strong>
-                                                        </div>
-                                                        <div class="col-8">
-                                                            <strong>{{ $menu->cantidad_packs }}</strong><span> packs disponibles</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                </div>
-                            @endforeach
-                        </div>
+                    <div class="container bg-body-primary">
+                        <input type="text" class="form-control" id="searchInputProvider" placeholder="Buscar...">
+                        <table id="dataTableProvider">
+                            <thead>
+                            <tr>
+                                <th>PROVEEDOR</th>
+                                <th>MENU</th>
+                                <th>PACKS</th>
+                            </tr>
+                            </thead>
+                            <tbody id="dataBodyProvider">
+                                @foreach ($providers as $provider)
+                                    @foreach ($provider->menus as $menu)
+                                        <tr data-idprovider="{{ $provider->id_prov }}" data-idmenu="{{ $menu->id_menu }}" data-nameMenu="{{ $menu->nombre_menu }}" data-cantidad="{{ $menu->cantidad_packs }}">
+                                            <td>{{ $provider->nombre }}</td>
+                                            <td class="nombre">{{ $menu->nombre_menu }}</td>
+                                            <td class="cantidad">{{ $menu->cantidad_packs }}</td>
+                                            </div>
+                                        </tr>
+                                    @endforeach
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
 
                 </div>
                 <div class="col">
                     <h3>SELECT RIDER</h3>
-                    @foreach ($riders as $rider)
-                        <div class="container bg-body-tertiary rider">
-                            {{ $rider->nombre }}
-                        </div>
-                    @endforeach
-
+                    <input type="text" class="form-control" id="searchInputRider" placeholder="Buscar...">
+                        <table id="dataTableRider">
+                            <thead>
+                            <tr>
+                                <th>NOMBRE</th>
+                            </tr>
+                            </thead>
+                            <tbody id="dataBodyRider">
+                                @foreach ($riders as $rider)
+                                    <tr data-idrider="{{ $rider->id_rider }}" data-nameRider="{{ $rider->nombre }}">
+                                        <td>{{ $rider->nombre }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                 </div>
                 <div class="col">
                     <h3>ADD PEDIDO</h3>
-                    <div class="card">
-                        <div class="card-body">
-                            <input type="hidden" name="idmenu" value="{{ $menu->id_menu }}"></input>
-                            <p class="card-text">{{ $menu->bebida }}</p>
-                            <p class="card-text">{{ $menu->plato1 }}</p>
-                            <p class="card-text">{{ $menu->plato2 }}</p>
-                            <div class="container text-center">
-                                <div class="row align-items-start">
-                                    <div class="col simbol decrement">
-                                        <span>-</span>
-                                    </div>
-                                    <div class="col">
-                                        <span class="quantity">0</span>
-                                        <input type="hidden" id="cantidad" name="cantidad"
-                                            value=""></input>
-                                        <input type="hidden" id="total"
-                                            value="{{ $menu->cantidad_packs }}"></input>
-                                    </div>
-                                    <div class="col simbol increment">
-                                        <span>+</span>
+                    <form action="{{ action([App\Http\Controllers\PedidoController::class, 'store']) }}" method="POST">
+                    @csrf
+                        <div class="card">
+                            <div class="card-body">
+                                <input type="hidden" id="idmenu" name="id_menu" value=""></input>
+                                <input type="hidden" id="idrider" name="id_rider" value=""></input>
+                                <input type="hidden" id="idprovider" name="id_provider" value=""></input>
+                                <input type="hidden" name="isadmin" value="true"></input>
+                                <input type="hidden" name="quantity" value=""></input>
+                                <p class="card-text">Selected menu: <strong id="menu"></strong></p>
+                                <p class="card-text">Selected rider: <strong id="rider"></strong></p>
+                                <div class="container text-center">
+                                    <div class="row align-items-start">
+                                        <div class="col simbol decrement">
+                                            <span>-</span>
+                                        </div>
+                                        <div class="col">
+                                            <span class="quantity">0</span>
+                                            <input type="hidden" id="cantidad" name="cantidad"
+                                                value=""></input>
+                                            <input type="hidden" id="total" value=""></input>
+                                        </div>
+                                        <div class="col simbol increment">
+                                            <span>+</span>
+                                        </div>
                                     </div>
                                 </div>
+                                <button type="submit" class="btn btn-primary" id="pedir">ADD</button>
                             </div>
-                            <button type="submit" class="btn btn-primary">ADD</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
